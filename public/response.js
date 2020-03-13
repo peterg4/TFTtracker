fb35289c453b44bce711dd85af52f384612ebd4b
peterg4 <43735968+peterg4@users.noreply.github.com>
2020-03-13 16:06:01 -0400

    skeleton for calls, weird permissions issue

--- public/response.js
+++ public/response.js
@@ -3,22 +3,14 @@
 
 // Instantiate the myApp Angular application that we attached to out html tag
 var app = angular.module("stonks", []);
-var call = 'https://na1.api.riotgames.com/tft/league/v1/grandmaster?api_key=RGAPI-386e0db4-3fd2-495f-8a91-7804274bc0af';
 // Here is the Javascript for our controller which we linked (scoped) to the body tag
 app.controller("stonk-controller", ['$scope','$http',function($scope, $http) {
     $scope.list = [];
     $scope.getItems = function() {
-        var call = 'https://na1.api.riotgames.com/tft/league/v1/grandmaster?api_key=RGAPI-386e0db4-3fd2-495f-8a91-7804274bc0af';
-        $http({method : 'GET',url : call})
-            .success(function(data, status) {
-                for(var i = 0; i < data.entries.length; i++) {
-                    list.push(data.entries[i].summonerName);
-                }
-            })
-            .error(function(data, status) {
-                $scope.list = [];
-                $scope.list.push("Bad API Call");
-            });       
+        $http.get("/challenger").then(function(data) {
+            // do something with the tracks
+            console.log(data);
+        })
     }
 }]);
 
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 var path = require('path');
 require('dotenv').config();
 var RiotRequest = require('riot-lol-api');
-var riotRequest = new RiotRequest('RGAPI-386e0db4-3fd2-495f-8a91-7804274bc0af');
+var riotRequest = new RiotRequest('RGAPI-0012d1dc-bf7b-4dd1-8765-f1bc9b8c7db4');
 
 app.use(express.static('public'));
 
@@ -17,12 +17,13 @@ app.get('/', function(req, res){
     res.sendFile('/index.html');
 });
 
-//app.get('/challenger')
-
+app.get('/challenger', function(req, res) {
+    riotRequest.request('na1', 'summoner', '/tft/league/v1/challenger', function(err, data) {
+        console.log(data);
+        res.json({entries: data});
+    });
+})
 
 http.listen(3000, function(){
     console.log('Server up on *:3000');
-    riotRequest.request('na1', 'summoner', '/tft/league/v1/challenger', function(err, data) {
-        console.log(data);
-    });
-  });
+ });
