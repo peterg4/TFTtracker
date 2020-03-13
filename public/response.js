0382ee7fc416b9683a74b9ec97637213ee1d9e8d
peterg4 <43735968+peterg4@users.noreply.github.com>
2020-03-13 16:30:19 -0400

    API connected for real

--- public/response.js
+++ public/response.js
@@ -6,10 +6,15 @@ var app = angular.module("stonks", []);
 // Here is the Javascript for our controller which we linked (scoped) to the body tag
 app.controller("stonk-controller", ['$scope','$http',function($scope, $http) {
     $scope.list = [];
+    $scope.tier = "";
     $scope.getItems = function() {
         $http.get("/challenger").then(function(data) {
             // do something with the tracks
             console.log(data);
+            $scope.tier = data.data.entries.tier;
+            for(var i = 0; i < data.data.entries.entries.length; i++) {
+                $scope.list.push(data.data.entries.entries[i].summonerName);
+            }
         })
     }
 }]);
