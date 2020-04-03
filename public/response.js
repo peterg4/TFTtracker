f7d762f948e0acf6150d5c82ab01c0c537505c4a
peterg4 <43735968+peterg4@users.noreply.github.com>
2020-04-03 10:29:00 -0400

    corrected tooltip for champions

--- public/response.js
+++ public/response.js
@@ -126,53 +126,66 @@ app.controller("stonk-controller", ['$scope','$http','$sce',function($scope, $ht
             $scope.search.push(data.data.entries.summonerLevel);
             $scope.search.push(data.data.entries.leaguePoints);
             $http.get('/traits.json').then(function(res){
-                for(var i = 0; i < 10; i++) {
-                    for(var j = 0; j < 8; j++) {
-                        if(data.data.entries[i].metadata.participants[j] == data.data.entries.puuid) {
-                            var entry = [];
-                            entry.push((Date.now()-data.data.entries[i].info.game_datetime)/3600000);
-                            entry.push("#" + data.data.entries[i].info.participants[j].placement);
-                            entry.push(data.data.entries[i].info.game_length/60);
-                            entry.push(data.data.entries[i].info.participants[j].level);
-                            entry.push(data.data.entries[i].info.participants[j].gold_left)
-                            entry.push(parseInt(data.data.entries[i].info.game_length%60));
-                            if(entry[5] < 10) {
-                                entry[5] = '0'+parseInt(entry[5]);
-                            }
-                            if(data.data.entries[i].info.queue_id == 1100) {
-                                entry.push('Ranked');
-                            } else {
-                                entry.push('Normal');
-                            }
-                            var sub_entry = [];
-                            for(var k = 0; k < data.data.entries[i].info.participants[j].units.length; k++) {
-                                sub_entry.push([data.data.entries[i].info.participants[j].units[k].character_id.substring(5).toLowerCase(),'border-rare'+data.data.entries[i].info.participants[j].units[k].rarity,data.data.entries[i].info.participants[j].units[k].character_id.substring(5)]);
-                            }
-                            entry.push(sub_entry);
-                            var trait_list = [];
-                            for(var k = 0; k < data.data.entries[i].info.participants[j].traits.length; k++) {
-                                if(data.data.entries[i].info.participants[j].traits[k].tier_current > 0) {
-                                    sub_entry = [];
-                                    sub_entry.push(data.data.entries[i].info.participants[j].traits[k].num_units);
-                                    sub_entry.push(data.data.entries[i].info.participants[j].traits[k].name);
-                                    sub_entry.push(data.data.entries[i].info.participants[j].traits[k].style);
-                                    sub_entry.push(sub_entry[1].toLowerCase());
-                                        for(var l = 0; l < res.data.length; l++) {
-                                            if(res.data[l].key == sub_entry[1]) {
-                                                sub_entry.push(res.data[l].name);
-                                                console.log(sub_entry);
-                                            }
-                                        }
-                                    trait_list.push(sub_entry);
+                $http.get('/champions.json').then(function(chars){
+                    console.log(chars);
+                    for(var i = 0; i < 10; i++) {
+                        for(var j = 0; j < 8; j++) {
+                            if(data.data.entries[i].metadata.participants[j] == data.data.entries.puuid) {
+                                var entry = [];
+                                entry.push((Date.now()-data.data.entries[i].info.game_datetime)/3600000);
+                                entry.push("#" + data.data.entries[i].info.participants[j].placement);
+                                entry.push(data.data.entries[i].info.game_length/60);
+                                entry.push(data.data.entries[i].info.participants[j].level);
+                                entry.push(data.data.entries[i].info.participants[j].gold_left)
+                                entry.push(parseInt(data.data.entries[i].info.game_length%60));
+                                if(entry[5] < 10) {
+                                    entry[5] = '0'+parseInt(entry[5]);
                                 }
+                                if(data.data.entries[i].info.queue_id == 1100) {
+                                    entry.push('Ranked');
+                                } else {
+                                    entry.push('Normal');
+                                }
+                                var sub_entry = [];
+                                for(var k = 0; k < data.data.entries[i].info.participants[j].units.length; k++) {
+                                    var subsub = [];
+                                    subsub.push(data.data.entries[i].info.participants[j].units[k].character_id.substring(5).toLowerCase());
+                                    subsub.push('border-rare'+data.data.entries[i].info.participants[j].units[k].rarity);
+                                    subsub.push(data.data.entries[i].info.participants[j].units[k].character_id.substring(5));
+                                    for(var l = 0; l < chars.data.length; l++) {
+                                        if(chars.data[l].championId == data.data.entries[i].info.participants[j].units[k].character_id) {
+                                            subsub.push(chars.data[l].name);
+                                           // console.log(sub_entry);
+                                        }
+                                    }
+                                    sub_entry.push(subsub);
+                                }
+                                entry.push(sub_entry);
+                                var trait_list = [];
+                                for(var k = 0; k < data.data.entries[i].info.participants[j].traits.length; k++) {
+                                    if(data.data.entries[i].info.participants[j].traits[k].tier_current > 0) {
+                                        sub_entry = [];
+                                        sub_entry.push(data.data.entries[i].info.participants[j].traits[k].num_units);
+                                        sub_entry.push(data.data.entries[i].info.participants[j].traits[k].name);
+                                        sub_entry.push(data.data.entries[i].info.participants[j].traits[k].style);
+                                        sub_entry.push(sub_entry[1].toLowerCase());
+                                            for(var l = 0; l < res.data.length; l++) {
+                                                if(res.data[l].key == sub_entry[1]) {
+                                                    sub_entry.push(res.data[l].name);
+                                                //    console.log(sub_entry);
+                                                }
+                                            }
+                                        trait_list.push(sub_entry);
+                                    }
+                                }
+                                trait_list.sort(sortFunction);
+                                entry.push(trait_list);
+                                $scope.matches.push(entry);
                             }
-                            trait_list.sort(sortFunction);
-                            entry.push(trait_list);
-                            $scope.matches.push(entry);
                         }
                     }
-                }
-                $scope.matches.sort(sortMatches);
+                    $scope.matches.sort(sortMatches);
+                })
             })
         })
     }
